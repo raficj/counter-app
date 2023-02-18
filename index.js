43ba5a84fe29c93b77162c89e24f0174d5e97492
Rafic <rjouejati@gmail.com>
2023-02-18 13:25:41 -0500

    Add counter app files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let counter = count + " - "
+    saveEl.textContent += counter
+    countEl.textContent = 0
+    count = 0
+}
+
+function reset() {
+    saveEl.textContent = "Previous entries: "
+    countEl.textContent = 0
+    count = 0
+}
